d4529bbcdccfeecc3c4e5af86a1c316522b1d3d0
Jason <jas0nle@yahoo.com>
2019-08-16 18:16:28 -0500

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        "#{self.first_name} #{self.last_name}"
+    end
+
+    def list_roles
+     char_role = "#{self.characters[0].name} - #{self.shows[0].name}" 
+     listroles = []
+     listroles << char_role  
+    end
+
+    
 end
